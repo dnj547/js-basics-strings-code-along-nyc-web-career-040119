27b7b0234bcaa6d81d5a3ecf955024208198d12d
dnj547 <dnj547@live.com>
2019-05-11 09:28:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Danielle';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
